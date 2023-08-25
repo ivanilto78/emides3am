e71ce7e1e2d11b263153cfcb5563ec160940d6d3
Ivanilto Santos <ivanilto.santos@aluno.senai.br>
2023-08-25 16:11:47 -0300

    CRUD

---
+++ ControledePet/ControledePet/Data/BancoContext.cs
@@ -0,0 +1,15 @@
+﻿using ControledePet.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Buffers.Text;
+
+namespace ControledePet.Data
+{
+    public class BancoContext:DbContext
+    {
+        public  BancoContext(DbContextOptions<BancoContext>options):base(options)
+        { 
+        }
+
+        public DbSet<PetModel> Pets { get; set; }
+    }
+}
---
+++ ControledePet/ControledePet/Repositorio/PetRepositorio.cs
@@ -0,0 +1,60 @@
+﻿using ControledePet.Data;
+using ControledePet.Models;
+
+namespace ControledePet.Repositorio
+{
+    public class PetRepositorio: IPetRepositorio
+    {
+
+        private readonly BancoContext _bancoContext;
+        public PetRepositorio(BancoContext bancoContext)
+        {
+            _bancoContext = bancoContext;
+        }
+        public List<PetModel> BuscarTodos()
+        {
+            return _bancoContext.Pets.ToList();
+        }
+
+        public PetModel Adicionar(PetModel pet)
+        {
+            //Gravar no banco
+            _bancoContext.Pets.Add(pet);
+            _bancoContext.SaveChanges();
+           return pet;
+        }
+
+        public PetModel ListarPorId(int id)
+        {
+           return _bancoContext.Pets.FirstOrDefault(x => x.Id == id);
+        }
+
+        public PetModel Atualizar(PetModel pet)
+        {
+            PetModel petDB = ListarPorId(pet.Id);
+            if (petDB == null) throw new SystemException("Houve um erro ao atualizar");
+
+            petDB.Nome = pet.Nome;
+            petDB.Genero = pet.Genero;
+            petDB.Raca = pet.Raca;
+
+            _bancoContext.Pets.Update(petDB);
+            _bancoContext.SaveChanges();
+ 
+            return petDB;
+
+        }
+
+        public bool Apagar(int id)
+        {
+            PetModel petDB = ListarPorId(id);
+            if (petDB == null) throw new SystemException("Houve um erro ao atualizar");
+
+          
+            _bancoContext.Pets.Remove(petDB);
+            _bancoContext.SaveChanges();
+
+            return true;
+        }
+    }
+}
